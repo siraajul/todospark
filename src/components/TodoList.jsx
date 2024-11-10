cd645f7f9b2fb7fbd93373f28235ce936a3abb07
siraajul <shirajulislamparvez@gmail.com>
2024-11-10 23:42:06 +0600

    Props Update

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,15 @@
 import React from "react";
+import TodoCard from "./TodoCard";
 
-export default function TodoList() {
-  let todos = ["Go to the gym", "Eat more fruits and veg"];
+export default function TodoList(props) {
+  const { todos } = props;
   return (
     <ul className="main">
       {todos.map((todo, todoIndex) => {
         return (
-          <li className="todoItem" key={todoIndex}>
-            {todo}
-            <i class="fa-solid fa-pen-to-square"></i>
-          </li>
+          <TodoCard key={todoIndex}>
+            <p>{todo}</p>
+          </TodoCard>
         );
       })}
     </ul>
