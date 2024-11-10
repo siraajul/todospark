4f8fd1311d1531a4a6fc6a2b24b8d197f4a2ef08
siraajul <shirajulislamparvez@gmail.com>
2024-11-10 23:24:34 +0600

    Create Input Place Holder

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function TodoInput() {
-  return <div>TodoInput</div>;
+  return (
+    <header>
+      <input placeholder="Enter ToDo...." />
+      <button>Add</button>
+    </header>
+  );
 }
