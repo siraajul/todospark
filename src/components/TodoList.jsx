5247ff72f56f3bbf6b68ddf6e7b549cff31a3f03
siraajul <shirajulislamparvez@gmail.com>
2024-11-11 00:11:47 +0600

    Delete Function Implement

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import TodoCard from "./TodoCard";
 
 export default function TodoList(props) {
-  const { todos } = props;
+  const { todos, handleDeleteTodo } = props;
   return (
     <ul className="main">
       {todos.map((todo, todoIndex) => {
         return (
-          <TodoCard key={todoIndex}>
+          <TodoCard {...props} key={todoIndex} index={todoIndex}>
             <p>{todo}</p>
           </TodoCard>
         );
