cf8066d6e46b10df39ba6963794903cf4c9b04ab
siraajul <shirajulislamparvez@gmail.com>
2024-11-10 23:24:43 +0600

    add todo item

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function TodoList() {
-  return <div>TodoList</div>;
+  let todos = ["Go to the gym", "Eat more fruits and veg"];
+  return (
+    <ul className="main">
+      {todos.map((todo, todoIndex) => {
+        return (
+          <li className="todoItem" key={todoIndex}>
+            {todo}
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
