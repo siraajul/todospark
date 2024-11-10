96ca5ff47bc03b2e015236df81e60e3c68a115f2
siraajul <shirajulislamparvez@gmail.com>
2024-11-10 23:56:39 +0600

    Update State

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function TodoInput() {
+export default function TodoInput(props) {
+  const { handleAddTodos } = props;
+  const [todoValue, setTodoValue] = useState("");
   return (
     <header>
-      <input placeholder="Enter ToDo...." />
-      <button>Add</button>
+      <input
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        placeholder="Enter ToDo...."
+      />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+        }}
+      >
+        Add
+      </button>
     </header>
   );
 }
